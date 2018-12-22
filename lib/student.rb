42caea80b6d17dbcf07df74a14a1ef1ebfb429f2
Jessica Gladstone <jcg321@comcast.net>
2018-12-22 18:20:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
   
   def self.create_from_collection(students_array)
        students_array.each do |person, attrib|
-          persons =  Student.new(person)
+       Student.new(person)
         end
   end
 
